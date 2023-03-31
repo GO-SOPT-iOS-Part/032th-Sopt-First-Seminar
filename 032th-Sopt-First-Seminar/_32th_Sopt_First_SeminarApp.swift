216c0740132fa83b104013dfa5e2b7a7f71ff0de
GodSamble <osgneb8359@naver.com>
2023-03-31 21:37:14 +0900

    Initial Commit

---
+++ 032th-Sopt-First-Seminar/_32th_Sopt_First_SeminarApp.swift
@@ -0,0 +1,17 @@
+//
+//  _32th_Sopt_First_SeminarApp.swift
+//  032th-Sopt-First-Seminar
+//
+//  Created by 고영민 on 2023/03/31.
+//
+
+import SwiftUI
+
+@main
+struct _32th_Sopt_First_SeminarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
