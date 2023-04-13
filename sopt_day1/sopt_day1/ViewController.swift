0dc08ab37f3655db4302c5b497d94979b75b7e9c
GodSamble <osgneb8359@naver.com>
2023-04-14 01:17:31 +0900

    2차세미나 실습 코드 커밋

--- sopt_day1/sopt_day1/ViewController.swift
+++ sopt_day1/sopt_day1/ViewController.swift
@@ -8,34 +8,34 @@
 import UIKit
 
 final class ViewController: UIViewController {
-    
+
     private let nameLabel: UILabel = {
         let label = UILabel()
         label.text = "솝트에 오신 여러분 환영합니다!"
         return label
     }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         setStyle()
         setLayout()
     }
 }
 
 private extension ViewController {
-    
+
     func setStyle() {
-        
+
         view.backgroundColor = .white
     }
-    
+
     func setLayout() {
-        
+
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
-        
+
         view.addSubview(nameLabel)
-        
+
         NSLayoutConstraint.activate([nameLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 300),
                                      nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50)])
     }
