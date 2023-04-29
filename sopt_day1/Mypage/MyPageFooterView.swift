05980ce3ca9a6de671cedbdcced1734eb23551f9
GodSamble <osgneb8359@naver.com>
2023-04-29 17:16:31 +0900

    [Chore, Add] #6 - BaseView, BaseViewController, 테이블뷰 폴더의 파일 세분화 작업

---
+++ sopt_day1/Mypage/MyPageFooterView.swift
@@ -0,0 +1,16 @@
+//
+//  MyPageFooterView.swift
+//  sopt_Seminar
+//
+//  Created by 고영민 on 2023/04/29.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+class MyPageFooterView: UIView {
+
+    private let bottombutton = UIButton()
+    
+}
